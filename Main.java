ceef9669d557661542ac59c35289be9abfcf8d4f
chamin96 <chaminmax@gmail.com>
2018-10-05 16:06:24 +0530

    Task 01

---
+++ Main.java
@@ -0,0 +1,7 @@
+//First program
+//Friday, October 5, 2018 3:40:02 PM | Author: E/15/154
+public class Main{
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
---
+++ Task01.java
@@ -0,0 +1,22 @@
+//Task 01
+//Friday, October 5, 2018 3:58:40 PM | Author: E/15/154
+
+import java.util.Scanner;  //import Scanner
+
+public class Task01{
+    public static void main(String[] args){
+        int num;
+
+        //create a new scanner
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        num = keyboard.nextInt();
+        System.out.println(num+" is special but not scary.");
+
+        System.out.print("Enter a number: ");
+        num = keyboard.nextInt();
+        System.out.println(num+" is special, weird and scary but not big.");
+
+    }
+}
