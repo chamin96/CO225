f6641c32e799b12f9955d392785a6877b94c6f64
Chamin Jayasooriya <33717507+chamin96@users.noreply.github.com>
2018-12-05 22:25:27 +0530

    added a matrix gen

--- lab3/code/Main.java
+++ lab3/code/Main.java
@@ -1,12 +1,20 @@
-class Main { 
-    public static int [][] a = {{1, 1, 1},
-				{1, 1, 1},
-				{1, 1, 1}};
+class Main {
+	public static int size=500;
+	public static int [][] a = new int[size][size];
+
     
     public static int [][] b = {{1, 1},
 				{1, 1},
 				{1, 1}};
 
+
+	public static void matrixGen(){
+		for(int i=0;i<size;i++){
+			for(int j=0;j<size;j++){
+				a[i][j]=(int)(Math.random()*1000);
+			}
+		}
+	}
 
     public static void print_matrix(int [][] a) {
 	for(int i=0; i < a.length; i++) {
@@ -19,8 +27,9 @@ class Main {
 
     public static void main(String [] args) {
 
+		matrixGen();
     	try{
-    		int [][] x = Matrix.multiply(a, b); 
+    		int [][] x = Matrix.multiply(a, a); 
 			print_matrix(x); // see if the multipication is correct
     	}
     	catch(NullPointerException e){
